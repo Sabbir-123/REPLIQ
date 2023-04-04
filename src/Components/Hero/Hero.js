296e62f27cd909918ee6303f46b956613ebc438c
Md Sabbir Ahmed <mdsabbirahmed@Mds-MacBook-Air.local>
2023-04-05 00:43:47 +0600

    first commit

---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            Hi
+        </div>
+    );
+};
+
+export default Hero;
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGZZ9sRdYPeWE2QwJvidzQN1JF4Ycflag",
+  authDomain: "repliq-55e05.firebaseapp.com",
+  projectId: "repliq-55e05",
+  storageBucket: "repliq-55e05.appspot.com",
+  messagingSenderId: "242011341338",
+  appId: "1:242011341338:web:2bc39fda02f59cb35a87a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
