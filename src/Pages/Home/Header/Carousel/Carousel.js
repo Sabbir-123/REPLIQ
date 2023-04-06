5d2b4e5f36eabda65c51d2f1bed5f33100d81a77
Md Sabbir Ahmed <mdsabbirahmed@Mds-MacBook-Air.local>
2023-04-06 15:32:25 +0600

    video uploaded

--- src/Pages/Home/Header/Carousel/Carousel.js
+++ src/Pages/Home/Header/Carousel/Carousel.js
@@ -1,27 +1,27 @@
 import React from 'react';
 
 const Carousel = () => {
-    const sliderImage = [
-        {
-            image: img1,
-            prev: 1,
-            id: 2,
-            next: 3
-        },
-        {
-            image: img3,
-            prev: 3,
-            id: 1,
-            next: 2
-        },
+    // const sliderImage = [
+    //     {
+    //         image: img1,
+    //         prev: 1,
+    //         id: 2,
+    //         next: 3
+    //     },
+    //     {
+    //         image: img3,
+    //         prev: 3,
+    //         id: 1,
+    //         next: 2
+    //     },
         
-        {
-            image: img2,
-            prev: 2,
-            id: 3,
-            next: 1
-        }
-    ]
+    //     {
+    //         image: img2,
+    //         prev: 2,
+    //         id: 3,
+    //         next: 1
+    //     }
+    // ]
 
     return (
         <div>
--- src/Pages/Home/Header/Header.js
+++ src/Pages/Home/Header/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Carousel from './Carousel/Carousel';
 import SideItem from './SideItem/SideItem';
-
+import Mock from '../../../Assets/Home/Mock.mp4'
 const Header = () => {
     return (
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1">
         <div className="col-span-2 lg:mb-[-170px] mb-2">
-          <Carousel></Carousel>
+          {/* <Carousel></Carousel> */}
+          <video controls>
+        <source src={Mock} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
         </div>
         <div className="lg:mb-[-170px] mb-2">
           <SideItem></SideItem>
